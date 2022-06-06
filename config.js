784cbe755e2bc7f7120d18796490bb4e583a8e7b
vinhlinh2k2 <67699218+vinhlinh2k2@users.noreply.github.com>
2022-06-06 14:14:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'bạn à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao bạn cứ đi nhầm đường
+    Lạc vào tim mình lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'nhấn vào đây',
+    title: 'Phải chăng bạn đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích mình <33',
+    btnNo: 'Không, bạn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33',
+    mess: 'mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h mình qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/vinhlinh26' 
+}
